fa70392a724080b0dd386530c7bca1ff3d8396cd
wangdahoo <157195705@qq.com>
2017-01-13 22:24:38 +0800

    update to vonic 1.1.1

--- template/src/components/About.vue
+++ template/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page has-navbar" v-nav="{title: 'About', showBackButton: true}">
     <div class="page-content text-center">
-      <p class="padding">Nothing here.</p>
+      <h2 class="padding">Nothing here.</h2>
     </div>
   </div>
 </template>
--- template/src/components/Index.vue
+++ template/src/components/Index.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="page has-navbar" v-nav="{ title: 'Home' }">
     <div class="page-content text-center">
-      <p class="padding">{{ msg }}</p>
+      <h2 class="padding" v-text="msg"></h2>
 
-      <a class="button button-assertive" v-link="{path: '/about'}">
-        about
-      </a>
+      <md-button class="button button-assertive" v-link="{path: '/about'}">
+        <i class="ion-information-circled"></i> About
+      </md-button>
     </div>
   </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Hello Vonic!'
+  export default {
+    data () {
+      return {
+        msg: 'Hello! Vonic.'
+      }
     }
   }
-}
 </script>
+<style>
+  h2 {
+    font-family: Candara,Calibri,Segoe,Segoe UI,Optima,Arial,sans-serif;
+    color: #888;
+  }
+
+  .page.has-navbar .page-content {
+    padding-top: 100px;
+  }
+</style>
