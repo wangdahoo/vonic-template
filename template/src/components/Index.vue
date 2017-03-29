7215d2471de5606fd38a40e65522f426dbbe6c9c
wangdahoo <157195705@qq.com>
2017-03-30 00:06:42 +0800

    update

--- template/src/components/Index.vue
+++ template/src/components/Index.vue
@@ -6,6 +6,29 @@
       <router-link class="button button-assertive" to="/about">
         <i class="ion-information-circled"></i> About
       </router-link>
+
+      <div style="height: 40px;"></div>
+
+      <swiper ref="swiper" 
+        direction="horizontal" 
+        width="100%" 
+        height="150" 
+        pager-color="#ea5a49" 
+        pager-bg-color="#fff"
+        hide-pager="false"
+      >
+        <swiper-item style="background: #0a9dc7;">
+          <h3>Keep Hungry</h3>
+        </swiper-item>
+
+        <swiper-item style="background: #44cc00;">
+          <h3>Keep Foolish</h3>
+        </swiper-item>
+
+        <swiper-item style="background: #ffc900;">
+          <h3>..</h3>
+        </swiper-item>
+      </swiper>
     </div>
   </div>
 </template>
@@ -22,6 +45,12 @@
   h2 {
     font-family: Candara,Calibri,Segoe,Segoe UI,Optima,Arial,sans-serif;
     color: #888;
+  }
+
+  h3 {
+    font-family: Candara,Calibri,Segoe,Segoe UI,Optima,Arial,sans-serif;
+    line-height: 150px;
+    color: #fff;
   }
 
   .page.has-navbar .page-content {
